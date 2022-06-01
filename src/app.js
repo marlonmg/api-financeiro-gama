a3170ff5b760a4910599cf1b37d4d140acd495da
Marlon Grazzianne <marlonmgm@hotmail.com>
2022-05-31 22:23:03 -0300

    refactor: Alterado para porta 5000

--- src/app.js
+++ src/app.js
@@ -16,4 +16,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(3000, console.log('Servidor rodando na porta 3000'));
+app.listen(5000, console.log('Servidor rodando na porta 3000'));
