c8e0ca68ff6cd3b27dab29b55c34e281875c72f2
Marlon Grazzianne <marlonmgm@hotmail.com>
2022-06-01 00:52:19 -0300

    refactor: teste heroku

--- src/app.js
+++ src/app.js
@@ -11,9 +11,14 @@ swaggerDocument = YAML.load("./swagger_aws.yaml");
 
 const app = express();
 
+const port = process.env.PORT || 3000
+
 //db.hasConection();
 
 app.use(express.json());
 app.use(routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(3000, console.log('Servidor rodando na porta 3000'));
+
+app.listen(port, () => {
+    console.info("Aplicacao rodando")
+})
