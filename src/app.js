2bc02af0d2417acc8d02ada968a523a4e33c889d
Marlon Grazzianne <marlonmgm@hotmail.com>
2022-05-31 22:33:07 -0300

    refactor: Ajuste heroku

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
 const db = require('./database');
-const swaggerUi = require("swagger-ui-express");
-const YAML = require("yamljs");
-swaggerDocument = YAML.load("./swagger_aws.yaml");
+
+//const swaggerUi = require("swagger-ui-express");
+//const YAML = require("yamljs");
+//swaggerDocument = YAML.load("./swagger_aws.yaml");
+
 //var cors = require("cors");
 //const compression = require('compression')
 
@@ -13,7 +15,9 @@ const app = express();
 
 //db.hasConection();
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.json());
 app.use(routes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(process.env.PORT || 3000, console.log('Servidor rodando na porta 3000'));
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.listen(PORT, console.log(`Servidor rodando na porta ${PORT}` ));
