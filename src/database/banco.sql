65a786dd966540076b48a42067df032808f41867
Marlon Grazzianne <56650153+marlonmg@users.noreply.github.com>
2022-06-04 13:40:29 -0300

    Update banco.sql

--- src/database/banco.sql
+++ src/database/banco.sql
@@ -1,84 +1,37 @@
--- MySQL Workbench Forward Engineering
+-- mydb.categoria definition
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`usuario` (
-  `idusuario` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `senha` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idusuario`))
-ENGINE = InnoDB;
+CREATE TABLE `categoria` (
+  `idcategoria` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(45) NOT NULL,
+  PRIMARY KEY (`idcategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`categoria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`categoria` (
-  `idcategoria` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NULL,
-  PRIMARY KEY (`idcategoria`))
-ENGINE = InnoDB;
+-- mydb.usuario definition
+
+CREATE TABLE `usuario` (
+  `idusuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
 
 
--- -----------------------------------------------------
--- Table `mydb`.`carteira`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`carteira` (
-  `idcarteira` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NOT NULL,
-  `valor` DOUBLE NOT NULL,
-  `data` DATE NOT NULL,
-  `categoria` VARCHAR(45) NOT NULL,
-  `tipo` INT NOT NULL,
-  `categoria_idcategoria` INT NOT NULL,
+-- mydb.carteira definition
+
+CREATE TABLE `carteira` (
+  `idcarteira` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(45) NOT NULL,
+  `valor` double NOT NULL,
+  `data` date NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `categoria_idcategoria` int NOT NULL,
+  `compartilha` int NOT NULL DEFAULT '0',
+  `idusuario` int NOT NULL,
+  `status` char(2) NOT NULL DEFAULT '1',
+  `idusuario_compartilha` int DEFAULT NULL,
   PRIMARY KEY (`idcarteira`),
-  INDEX `fk_carteira_categoria_idx` (`categoria_idcategoria` ASC) VISIBLE,
-  CONSTRAINT `fk_carteira_categoria`
-    FOREIGN KEY (`categoria_idcategoria`)
-    REFERENCES `mydb`.`categoria` (`idcategoria`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`carteira_usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`carteira_usuario` (
-  `carteira_idcarteira` INT NOT NULL,
-  `usuario_idusuario` INT NOT NULL,
-  PRIMARY KEY (`carteira_idcarteira`, `usuario_idusuario`),
-  INDEX `fk_carteira_has_usuario_usuario1_idx` (`usuario_idusuario` ASC) VISIBLE,
-  INDEX `fk_carteira_has_usuario_carteira1_idx` (`carteira_idcarteira` ASC) VISIBLE,
-  CONSTRAINT `fk_carteira_has_usuario_carteira1`
-    FOREIGN KEY (`carteira_idcarteira`)
-    REFERENCES `mydb`.`carteira` (`idcarteira`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_carteira_has_usuario_usuario1`
-    FOREIGN KEY (`usuario_idusuario`)
-    REFERENCES `mydb`.`usuario` (`idusuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+  KEY `fk_carteira_categoria_idx` (`categoria_idcategoria`),
+  CONSTRAINT `fk_carteira_categoria` FOREIGN KEY (`categoria_idcategoria`) REFERENCES `categoria` (`idcategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb3;
